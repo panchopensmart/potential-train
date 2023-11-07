478a984e22f73eddc0433909ddc28f590942fcd6
Alexandr <a.polupanov@etpgpb.ru>
2023-11-07 16:33:33 +0300

    edited composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'a.polupanov/project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e85bff13854437c0711a5b5be342db0831a483e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'a.polupanov/project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e85bff13854437c0711a5b5be342db0831a483e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
